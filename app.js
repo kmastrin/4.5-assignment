0cfdc9a855dcaf16843814aba96e34ce7ba21b54
kmastrin <kmastrin@student.alamo.edu>
2023-11-07 17:00:19 -0600

    index

--- app.js
+++ app.js
@@ -17,3 +17,34 @@ if (grade >= 90) {
 } else {
   console.log("F");
 }
+
+const dayOfWeek = 3;
+let dayName;
+
+switch (dayOfWeek) {
+  case 1:
+    dayName = "Sunday";
+    break;
+  case 2:
+    dayName = "Monday";
+    break;
+  case 3:
+    dayName = "Tuesday";
+    break;
+  case 4:
+    dayName = "Wednesday";
+    break;
+  case 5:
+    dayName = "Thursday";
+    break;
+  case 6:
+    dayName = "Friday";
+    break;
+  case 7:
+    dayName = "Saturday";
+    break;
+  default:
+    dayName = "Invalid day";
+}
+
+console.log(`Today is ${dayName}.`);
