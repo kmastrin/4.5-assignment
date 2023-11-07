9d9f378d6e4e1bdca940dbba7bdaa476692888d6
kmastrin <kmastrin@student.alamo.edu>
2023-11-07 16:54:54 -0600

    index

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const lastName = "Hwang";
 const age = 23;
 
 // Create a concatenated string variable
-const concatenatedString = "My name is " + firstName + " " + lastName + " and I am " + age + " years old.";
+const concatenatedString = "My favorite kpop idol is " + firstName + " " + lastName + " and he is " + age + " years old.";
 
 // Output the concatenated string to the console
 console.log(concatenatedString);
