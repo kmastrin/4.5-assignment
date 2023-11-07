b3f8a22e79a82595ffad0c9e27a3cd6bef75f785
kmastrin <kmastrin@student.alamo.edu>
2023-11-07 16:52:36 -0600

    index

---
+++ app.js
@@ -0,0 +1,9 @@
+const firstName = "Hyunjin";
+const lastName = "Hwang";
+const age = 23;
+
+// Create a concatenated string variable
+const concatenatedString = "My name is " + firstName + " " + lastName + " and I am " + age + " years old.";
+
+// Output the concatenated string to the console
+console.log(concatenatedString);
